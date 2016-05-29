271052e783a595f114554c8af5d1ca4bd49fc275
Elena Nazarova <ln15ln@rambler.ru>
2016-05-30 01:43:34 +0400

    Add jQuery lib and set of main buttons

--- ui/index.php
+++ ui/index.php
@@ -15,29 +15,75 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>UB Hotspots</title>
+    <title>Игра с камнями</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <link type="text/css" rel="stylesheet" href="vis-folder/vis.css"/>
+    <link type="text/css" rel="stylesheet" href="display-styles.css"/>
     <script type="text/javascript" src="vis-folder/vis.js"></script>
-
+    <script type="text/javascript" src="jquery.js"></script>
 
     <?php
-    $var = 10;
-    if ($var == 1) {
-        echo '<b>Привет, Витек!</b>';
+
+
+    function get_string($identifier, $langfile){//взять аналог из мудла
+        //$langfile = "qtype_stonesgame.php";
+        $string = array();
+        include ($langfile.".php");
+        $strings[$langfile] = $string;
+        $string = &$strings[$langfile];
+
+        if (!isset ($string[$identifier])) {
+            return false;
+        }
+
+        return "$string[$identifier]";
     }
-    else
-    {
-        echo '<b>Привет, Евгений!</b>';
-    }
+
+
+
 ?>
 
+
+    <script type="application/javascript">
+        $("#buttonAdd").click(addElementAcc);
+
+        function addElementAcc() {
+            alert("вышло!!11");
+        }
+
+
+    </script>
 </head>
 <body>
 
+<div id="outerbl" style="position: absolute; width: 800px; left: 20%; background-color: #0d9ffe;">
+fff
+
+<div id="question" style="width: 100%; height: 100%;">
+    <div id="tree" class="tree">
 
 
 
+    </div>
+
+    <div id="buttons" class="right">
+        <button id="buttonAdd" class="main"><?php echo get_string('jsadd','qtype_stonesdebug'); ?></button>
+        <button id="buttonEdit" class="main"><?php echo get_string('jsedit','qtype_stonesdebug'); ?></button>
+        <button id="buttonDelete" class="main"><?php echo get_string('jsdelete','qtype_stonesdebug'); ?></button>
+        <button id="buttonMark" class="main"><?php echo get_string('jsmark','qtype_stonesdebug'); ?></button>
+        <button id="buttonEnd" class="main"><?php echo get_string('jsend','qtype_stonesdebug'); ?></button>
+        <button id="buttonClearAll" class="main"><?php echo get_string('jsclearall','qtype_stonesdebug'); ?></button>
+
+        <br><br><br>
+
+        <button id="buttonSave" class="main"><?php echo get_string('jssave','qtype_stonesdebug'); ?></button>
+        <a href="help/stonesgame.html" class="main" target="_blank"><?php echo get_string('jshelp','qtype_stonesdebug'); ?></a>
+    </div>
+
+
+</div>
+
+</div>
 
 
 </body>
---
+++ ui/qtype_stonesdebug.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The language strings for the stonesgame question type.
+ *    
+ * @copyright &copy; 2016 SSAU
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package stonesgame
+ *
+ */
+
+$string['pluginname'] = 'Задание C3';
+$string['editingstonesgame'] = 'Редактирование задания C3';
+$string['addingstonesgame'] = 'Добавление задания C3';
+$string['stonesgame'] = 'Задание C3';
+$string['openeditor'] = 'Открыть окно для построения дерева (Задание 3)';
+$string['failedloadinganswers'] = "Возникли проблемы при загрузке ответов..";
+
+// HS Editor Javascript strings
+
+$string['jsadd'] = 'Сделать ход';
+$string['jsedit'] = 'Изменить ход';
+$string['jsdelete'] = 'Удалить ход';
+$string['jsmark'] = 'Отметить ходы выигрышной стратегии';
+$string['jsend'] = 'Отметить ход как заключительный';
+$string['jsclearall'] = 'Очистить экран';
+
+$string['jsunmark'] = 'Снять все метки';
+$string['jssavemarks'] = 'Сохранить выигрышную стратегию';
+
+$string['jssave'] = 'Сохранить';
+$string['jshelp'] = 'Справочная информация';
+
+$string['jsok'] = 'Да';
+$string['jsno'] = 'Нет';
+
+$string['jsclosewithout'] = 'Закрыть окно без сохранения изменений?';
+$string['jswarning'] = 'Внимание!';
+$string['jsconfirmdelete'] = 'Удалить ход?';
+
+?>
