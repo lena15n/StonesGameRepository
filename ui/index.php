4a0b8ca96edbcad7b11baafb568cbb55ebf4bff1
Elena Nazarova <ln15ln@rambler.ru>
2016-05-30 21:36:52 +0400

    Add popup with default buttons Add, Edit..

--- ui/index.php
+++ ui/index.php
@@ -38,26 +38,89 @@
         return "$string[$identifier]";
     }
 
-
+    //TODO: в отдельном классе .php (т к безопасность) реализовываем логику, здесь же вызываем и в функциях js рисуем вершины
     ?>
 
 
     <script type="application/javascript">
-        $("#buttonAdd").click(addElementAcc);
+/*
+        <
+        div
+        class
+        = "vis-network"
+        tabindex = "900"
+        style = "position: relative; overflow: hidden; touch-action: pan-y; -webkit-user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); width: 100%; height: 100%;" > < canvas
+        width = "800"
+        height = "600"
+        style = "position: relative; touch-action: none; -webkit-user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); width: 100%; height: 100%;" > < / canvas > < div
+        class
+        = "vis-manipulation"
+        style = "display: block;" > < div
+        class
+        = "vis-button vis-add"
+        style = "touch-action: pan-y; -webkit-user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" > < div
+        class
+        = "vis-label" > Add
+        Node < / div > < /
+        div > < div
+        class
+        = "vis-separator-line" > < / div > < div
+        class
+        = "vis-button vis-connect"
+        style = "touch-action: pan-y; -webkit-user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" > < div
+        class
+        = "vis-label" > Add
+        Edge < / div > < /
+        div > < / div > < div
+        class
+        = "vis-edit-mode"
+        style = "display: none;" > < / div > < div
+        class
+        = "vis-close"
+        style = "display: block; touch-action: pan-y; -webkit-user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);" > < / div > < /
+        div >
+*/
 
-        function addElementAcc() {
-            alert("вышло!!11");
+        function addState() {
+            //alert("вышло!!11");
+
+
+        }
+
+        function editState() {
+
+
+        }
+
+        function deleteState() {
+
+        }
+
+        function setWinStrategy() {
+
+        }
+
+        function markStateAsWin() {
+
+        }
+
+        function clearAll() {
+
+        }
+
+        function saveAll() {
+
         }
 
 
         var nodes = null;
         var edges = null;
-        var network = null;
+        var tree = null;
 
         function destroy() {
-            if (network !== null) {
-                network.destroy();
-                network = null;
+            if (tree !== null) {
+                tree.destroy();
+                tree = null;
             }
         }
 
@@ -68,41 +131,68 @@
             var connectionCount = [];
 
             // randomly create some nodes and edges
-            for (var i = 0; i < 15; i++) {
+            for (var i = 0; i < 10; i++) {
                 nodes.push({id: i, label: String(i)});
             }
-            edges.push({from: 0, to: 1});
-            edges.push({from: 0, to: 6});
-            edges.push({from: 0, to: 13});
-            edges.push({from: 0, to: 11});
-            edges.push({from: 1, to: 2});
-            edges.push({from: 2, to: 3});
-            edges.push({from: 2, to: 4});
-            edges.push({from: 3, to: 5});
-            edges.push({from: 1, to: 10});
-            edges.push({from: 1, to: 7});
-            edges.push({from: 2, to: 8});
-            edges.push({from: 2, to: 9});
-            edges.push({from: 3, to: 14});
-            edges.push({from: 1, to: 12});
+
+            edges.push({from: 0, to: 1, arrows: 'to'});
+            edges.push({from: 1, to: 2, arrows: 'to'});
+            edges.push({from: 2, to: 3, arrows: 'to'});
+            edges.push({from: 2, to: 4, arrows: 'to'});
+            edges.push({from: 2, to: 5, arrows: 'to'});
+            edges.push({from: 2, to: 6, arrows: 'to'});
+            edges.push({from: 6, to: 7, arrows: 'to'});
+            edges.push({from: 5, to: 8, arrows: 'to'});
+            edges.push({from: 4, to: 9, arrows: 'to'});
             nodes[0]["level"] = 0;
             nodes[1]["level"] = 1;
-            nodes[2]["level"] = 3;
-            nodes[3]["level"] = 4;
-            nodes[4]["level"] = 4;
-            nodes[5]["level"] = 5;
-            nodes[6]["level"] = 1;
-            nodes[7]["level"] = 2;
+            nodes[2]["level"] = 2;
+            nodes[3]["level"] = 3;
+            nodes[4]["level"] = 3;
+            nodes[5]["level"] = 3;
+            nodes[6]["level"] = 3;
+            nodes[7]["level"] = 4;
             nodes[8]["level"] = 4;
             nodes[9]["level"] = 4;
-            nodes[10]["level"] = 2;
-            nodes[11]["level"] = 1;
-            nodes[12]["level"] = 2;
-            nodes[13]["level"] = 1;
-            nodes[14]["level"] = 5;
+            
 
 
-            // create a network
+            nodes[0].label = "(7, 31)\n38";
+            nodes[0].color = "#FFFFFF";
+            nodes[0].shape = "box";
+
+            nodes[1].label = "(7, 32)\n39";
+
+            nodes[2].label = "(8, 32)\n40";
+            nodes[2].color = "#00FFFF";
+
+            nodes[3].label = "(9, 32)\n41";
+
+            nodes[4].label = "(8, 33)\n41";
+
+            nodes[5].label = "(16, 32)\n48";
+
+            nodes[6].label = "(8, 64)\n72";
+
+            nodes[7].label = "(8, 128)\n136";
+            nodes[7].color = "#00FFFF";
+            nodes[7].borderWidth = "4";
+            nodes[7].color.border = "#00FF00";
+
+            nodes[8].label = "(16, 64)\n80";
+            nodes[8].color = "#00FFFF";
+            nodes[8].borderWidth = "4";
+            $(nodes[8]).color = "red";
+
+            nodes[9].label = "(8, 66)\n74";
+            nodes[9].color = "#00FFFF";
+            nodes[9].borderWidth = "4";
+            nodes[9].color.border = "#00FF00";
+            
+            
+            
+            
+            // create a tree
             var container = document.getElementById('tree');
             var data = {
                 nodes: nodes,
@@ -122,12 +212,23 @@
                         direction: 'LR'
                     }
                 },
+                manipulation: {
+                    enabled: true,
+                    initiallyActive: true,
+                    addNode: true,
+                    addEdge: true,
+                    editNode: undefined,
+                    editEdge: true,
+                    deleteNode: true,
+                    deleteEdge: true
+                },
                 physics: false
             };
-            network = new vis.Network(container, data, options);
+
+            tree = new vis.Network(container, data, options);
 
             // add event listeners
-            network.on('select', function (params) {
+            tree.on('select', function (params) {
                 document.getElementById('selection').innerHTML = 'Selection: ' + params.nodes;
             });
 
@@ -135,6 +236,11 @@
 
         $(document).ready(function () {
             draw();
+
+
+            $("#buttonAdd").click(addState);
+
+
         });
 
     </script>
@@ -143,11 +249,11 @@
 </head>
 <body>
 
-<div id="outerbl" style="position: absolute; width: 850px; left: 20%; border: solid; border-color: #f79646">
+<div id="outerbl" style="position: absolute; width: 850px; left: 20%;  ">
     <p class="maintext"><i>Текст задания C3</i></p>
 
     <div id="question" style="width: 100%; height: 100%;">
-        <div id="treeAndButtons" style="width: 850px; height: 400px; border: solid; border-color: #00ff26">
+        <div id="treeAndButtons" style="width: 850px; height: 400px;  ">
             <div id="tree" class="tree">
             </div>
 
